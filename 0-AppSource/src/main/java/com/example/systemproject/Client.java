b38292013ef4794f8163a1e40de0ff67e5f8ec43
iAloudat <110925225+iAloudat@users.noreply.github.com>
2023-02-13 15:05:29 -0500

    Update Client

--- 0-AppSource/src/main/java/com/example/systemproject/Client.java
+++ 0-AppSource/src/main/java/com/example/systemproject/Client.java
@@ -1,4 +1,59 @@
 package com.example.systemproject;
 
 public class Client {
+    private int client_id;
+    private String client_name;
+    private String company_name;
+    private String tel;
+    private String email;
+
+    public Client() {
+    }
+    public Client(int client_id, String client_name, String company_name, String tel, String email) {
+        this.client_id = client_id;
+        this.client_name = client_name;
+        this.company_name = company_name;
+        this.tel = tel;
+        this.email = email;
+    }
+
+    public int getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(int client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getClient_name() {
+        return client_name;
+    }
+
+    public void setClient_name(String client_name) {
+        this.client_name = client_name;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
